aea25d964f5a52ef02a11ce51ae9fb54bc0c0e5a
Mustafa <mustafa@shipbob.com>
2018-05-02 13:30:25 -0500

    update to nuget

--- PreflightHQ.ActionExecutor/ActionExecuter.cs
+++ PreflightHQ.ActionExecutor/ActionExecuter.cs
@@ -23,42 +23,46 @@ namespace PreflightHQ.ActionExecutor
             driverWait.Until(d =>
                 ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
         }
+        public void Execute(ClientAction action)
+        {
+            WaitUntilReady();
+            driverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            var by = action.Selector == null ? null : By.CssSelector(action.Selector);
+            if (by != null)
+                driverWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+            switch (action.ActionType)
+            {
+                case ActionType.Click:
+                    driver.FindElement(by).Click();
+                    break;
+                case ActionType.Type:
+                    SendKeysToElement(by, action.Value);
+                    break;
+                case ActionType.Navigate:
+                    driver.Url = action.Value;
+                    break;
+                case ActionType.SetCheckpoint:
+                    var elementText = driver.FindElement(by).Text;
+                    var expectedText = System.Net.WebUtility.HtmlDecode(action.Value);
+
+                    if (expectedText != elementText)
+                    {
+                        throw new ApplicationException($"'{action.Value}' is not equal to '{expectedText}' at {by.ToString()}");
+                    }
+                    break;
+                case ActionType.Enter:
+                    driver.FindElement(by).SendKeys(Keys.Enter);
+                    break;
+                case ActionType.Select:
+                    new SelectElement(driver.FindElement(by)).SelectByText(action.Value);
+                    break;
+            }
+        }
         public void Execute(List<ClientAction> actions)
         {
             foreach (var action in actions)
             {
-                WaitUntilReady();
-                driverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-                var by = action.Selector == null ? null : By.CssSelector(action.Selector);
-                if (by != null)
-                    driverWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(by));
-                switch (action.ActionType)
-                {
-                    case ActionType.Click:
-                        driver.FindElement(by).Click();
-                        break;
-                    case ActionType.Type:
-                        SendKeysToElement(by, action.Value);
-                        break;
-                    case ActionType.Navigate:
-                        driver.Url = action.Value;
-                        break;
-                    case ActionType.SetCheckpoint:
-                        var elementText = driver.FindElement(by).Text;
-                        var expectedText = System.Net.WebUtility.HtmlDecode(action.Value);
-
-                        if (expectedText != elementText)
-                        {
-                            throw new ApplicationException($"'{action.Value}' is not equal to '{expectedText}' at {by.ToString()}");
-                        }
-                        break;
-                    case ActionType.Enter:
-                        driver.FindElement(by).SendKeys(Keys.Enter);
-                        break;
-                    case ActionType.Select:
-                        new SelectElement(driver.FindElement(by)).SelectByText(action.Value);
-                        break;
-                }
+                Execute(action);
             }
         }
       
